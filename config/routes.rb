5cd3f61c8db6f43ad6a6714f8a44cce1b7b6c965
Albert Ho <albertcyi.ho@gmail.com>
2022-03-10 11:04:00 -0800

    feat - add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
-
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -22,6 +19,14 @@ Rails.application.routes.draw do
   end
 
   resources :about, only: [:index]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
