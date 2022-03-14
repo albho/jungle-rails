82fa6645dc8b4ca3cdf7d45ac30e2fa6a93c65c6
Albert Ho <albertcyi.ho@gmail.com>
2022-03-14 15:39:10 -0700

    feat - add authenticate class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
-
   has_secure_password
-  
+
   validates :name, presence: true
   validates :password, presence: true, length: { minimum: 4 }
   validates :password_confirmation, presence: true
-  validates :email, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  def self.authenticate_with_credentials email, password
+    user = self.find_by_email email
+
+    return user if user && user.authenticate(password)
+    
+    nil
+  end
 
 end
