1ba7752aa75bc75df53c47597e4417671f284738
Albert Ho <albertcyi.ho@gmail.com>
2022-03-15 10:44:59 -0700

    test - add tests for clicking on a product

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
-  # SETUP
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  
   before :each do
     @category = Category.create! name: 'Apparel'
 
@@ -17,12 +16,13 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They click on a product" do
     visit root_path
 
     # commented out b/c it's for debugging only
-    # save_and_open_screenshot
+    first('h4').click
 
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css 'article.product-detail', count: 1
+    save_and_open_screenshot
   end
 end
